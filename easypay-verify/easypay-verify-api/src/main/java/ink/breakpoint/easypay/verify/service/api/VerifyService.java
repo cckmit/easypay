70cbee1bd5d8267d445991bfbcd5a0ae0ed044fd
shaoshuai <qinshaoshuai@aliyun.com>
2021-09-30 16:35:45 +0800

    Changes

--- easypay-verify/easypay-verify-api/src/main/java/ink/breakpoint/easypay/verify/service/api/VerifyService.java
+++ easypay-verify/easypay-verify-api/src/main/java/ink/breakpoint/easypay/verify/service/api/VerifyService.java
@@ -1,5 +1,7 @@
 package ink.breakpoint.easypay.verify.service.api;
 
 public interface VerifyService {
+    String sendVerifyCode(String phone);
 
+    void checkVerifyCode(String verifyKey,String verifyCode);
 }
